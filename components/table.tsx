37846b55c9bf165680e0347da79419941c89f955
ksjak7 <alecrwatkins@gmail.com>
2024-06-14 17:44:12 -0500

    init

--- components/table.tsx
+++ components/table.tsx
@@ -6,59 +6,23 @@ import { seed } from '@/lib/seed'
 
 export default async function Table() {
   let data
-  let startTime = Date.now()
 
-  try {
-    data = await sql`SELECT * FROM users`
-  } catch (e: any) {
-    if (e.message.includes('relation "users" does not exist')) {
-      console.log(
-        'Table does not exist, creating and seeding it with dummy data now...'
-      )
-      // Table is not created yet
-      await seed()
-      startTime = Date.now()
-      data = await sql`SELECT * FROM users`
-    } else {
-      throw e
-    }
-  }
+  await seed()
+  data = await sql`SELECT * FROM songtags`
+  const { rows: songs } = data
 
-  const { rows: users } = data
-  const duration = Date.now() - startTime
 
   return (
     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
       <div className="flex justify-between items-center mb-4">
         <div className="space-y-1">
-          <h2 className="text-xl font-semibold">Recent Users</h2>
-          <p className="text-sm text-gray-500">
-            Fetched {users.length} users in {duration}ms
-          </p>
+          <h2 className="text-xl font-semibold">Songs</h2>
         </div>
         <RefreshButton />
       </div>
       <div className="divide-y divide-gray-900/5">
-        {users.map((user) => (
-          <div
-            key={user.name}
-            className="flex items-center justify-between py-3"
-          >
-            <div className="flex items-center space-x-4">
-              <Image
-                src={user.image}
-                alt={user.name}
-                width={48}
-                height={48}
-                className="rounded-full ring-1 ring-gray-900/5"
-              />
-              <div className="space-y-1">
-                <p className="font-medium leading-none">{user.name}</p>
-                <p className="text-sm text-gray-500">{user.email}</p>
-              </div>
-            </div>
-            <p className="text-sm text-gray-500">{timeAgo(user.createdAt)}</p>
-          </div>
+        {songs.map((song) => (
+          <iframe src={`https://open.spotify.com/embed/track/${song.song_id}?utm_source=generator`} width="100%" height="352" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         ))}
       </div>
     </div>
