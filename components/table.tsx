732be3734f7412fbf38bc22903f5aca4b608c484
ksjak7 <alecrwatkins@gmail.com>
2024-06-14 17:51:57 -0500

    songs added

--- components/table.tsx
+++ components/table.tsx
@@ -1,6 +1,4 @@
 import { sql } from '@vercel/postgres'
-import { timeAgo } from '@/lib/utils'
-import Image from 'next/image'
 import RefreshButton from './refresh-button'
 import { seed } from '@/lib/seed'
 
--- lib/seed.ts
+++ lib/seed.ts
@@ -7,13 +7,19 @@ export async function seed() {
         id TEXT PRIMARY KEY NOT NULL,
         title TEXT NOT NULL
       )
-      `,
+    `,
     sql`
       CREATE TABLE IF NOT EXISTS songtags (
         song_id TEXT NOT NULL,
         tag TEXT NOT NULL,
         FOREIGN KEY(song_id) REFERENCES songs(id)
       )
-      `,
+    `,
+    sql`
+      INSERT INTO songs (id, title) VALUES ('6AI3ezQ4o3HUoP6Dhudph3', 'Not Like Us')
+    `,
+    sql`
+      INSERT INTO songtags (song_id, tag) VALUES ('6AI3ezQ4o3HUoP6Dhudph3', 'rap')
+    `
   ])
 }
