b286d0c52a7572c4c8030ea05acd895a424a8fdb
ksjak7 <alecrwatkins@gmail.com>
2024-06-23 00:18:00 -0500

    remove db creation

--- lib/seed.ts
+++ lib/seed.ts
@@ -2,18 +2,5 @@ import { sql } from '@vercel/postgres'
 
 export async function seed() {
   await Promise.all([
-    sql`
-      CREATE TABLE IF NOT EXISTS songs (
-        id TEXT PRIMARY KEY NOT NULL,
-        title TEXT NOT NULL
-      )
-    `,
-    sql`
-      CREATE TABLE IF NOT EXISTS songtags (
-        song_id TEXT NOT NULL,
-        tag TEXT NOT NULL,
-        FOREIGN KEY(song_id) REFERENCES songs(id)
-      )
-    `
   ])
 }
